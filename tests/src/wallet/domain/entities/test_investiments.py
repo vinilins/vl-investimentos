1ec0346975abb81b24fb1aeff8c76ba315d4c0ae
Vinícius Lins <viniciusaclins@gmail.com>
2024-05-05 16:28:37 -0300

    tests(wallet/domain): Creating tests for wallet context

---
+++ tests/src/wallet/domain/entities/test_investiments.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from decimal import Decimal
+
+from src.wallet.domain.entities.investments import Investment
+from src.wallet.domain.value_objects.assets import AssetsRepository, AssetType
+
+
+def test_create_investiments():
+    asset_name = "BBAS3"
+
+    asset = AssetsRepository().get_asset(name=asset_name, type=AssetType.STOCK)
+
+    bbas3 = Investment(
+        asset=asset, purchase_date=datetime(2024, 1, 1), quantity=14, unit_price="28.56"
+    )
+
+    assert bbas3.asset.name == "BBAS3"
+    assert bbas3.asset.type == AssetType.STOCK
+    assert bbas3.purchase_date == datetime(2024, 1, 1)
+    assert bbas3.quantity == 14
+    assert bbas3.unit_price == Decimal("28.56")
+    assert bbas3.total_price == Decimal("399.84")

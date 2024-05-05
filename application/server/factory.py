7ed8319feb190c49906f5fe07904ad95b91cc7e3
Vinícius Lins <viniciusaclins@gmail.com>
2024-05-05 14:42:01 -0300

    fix(application/server): Adding openapi to project

--- application/server/factory.py
+++ application/server/factory.py
@@ -5,9 +5,6 @@ def create_api_app():
     app = FastAPI(
         title="VL Investimentos",
         version="0.0.1",
-        docs_url=None,
-        redoc_url=None,
-        openapi_url=None,
     )
 
     @app.get("/")

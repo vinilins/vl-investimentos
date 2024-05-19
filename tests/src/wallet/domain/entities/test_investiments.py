d8a5447b654c7889fd91dcb82e4c58def7e35015
Vinícius Lins <viniciusaclins@gmail.com>
2024-05-19 19:18:29 -0300

    tests(wallet/domain/entities): Creating more tests to investiments entity

--- tests/src/wallet/domain/entities/test_investiments.py
+++ tests/src/wallet/domain/entities/test_investiments.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from decimal import Decimal
 
+import pytest
+from beartype.roar import BeartypeCallHintParamViolation
+
 from src.wallet.domain.entities.investments import Investment
 from src.wallet.domain.value_objects.assets import AssetsRepository, AssetType
 
@@ -23,3 +26,54 @@ def test_create_investiments():
     assert bbas3.quantity == 14
     assert bbas3.unit_price == Decimal("28.56")
     assert bbas3.total_price == Decimal("399.84")
+
+
+def test_create_investiments_asset_validation_error():
+    asset = "BBAS3"
+
+    with pytest.raises(BeartypeCallHintParamViolation):
+        Investment(
+            asset=asset,
+            purchase_date=datetime(2024, 1, 1),
+            quantity=14,
+            unit_price=Decimal("28.56"),
+        )
+
+
+def test_create_investiments_purchase_date_validation_error():
+    asset_name = "BBAS3"
+    asset = AssetsRepository().get_asset(name=asset_name, asset_type=AssetType.STOCK)
+
+    with pytest.raises(BeartypeCallHintParamViolation):
+        Investment(
+            asset=asset,
+            purchase_date="2024/05/19",
+            quantity=14,
+            unit_price=Decimal("28.56"),
+        )
+
+
+def test_create_investiments_quantity_validation_error():
+    asset_name = "BBAS3"
+    asset = AssetsRepository().get_asset(name=asset_name, asset_type=AssetType.STOCK)
+
+    with pytest.raises(BeartypeCallHintParamViolation):
+        Investment(
+            asset=asset,
+            purchase_date=datetime(2024, 1, 1),
+            quantity="14",
+            unit_price=Decimal("28.56"),
+        )
+
+
+def test_create_investiments_unit_price_validation_error():
+    asset_name = "BBAS3"
+    asset = AssetsRepository().get_asset(name=asset_name, asset_type=AssetType.STOCK)
+
+    with pytest.raises(BeartypeCallHintParamViolation):
+        Investment(
+            asset=asset,
+            purchase_date=datetime(2024, 1, 1),
+            quantity=14,
+            unit_price=28.56,
+        )
